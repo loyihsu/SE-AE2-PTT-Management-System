a9234b4430c468a97aea195104bd1b67a174d059
loyihsu <loyi.hsu@gmail.com>
2021-05-18 18:50:43 +0800

    Add another list named assignments to handle assignment of teaching staff and lab.

---
+++ src/datatype/Assignment.java
@@ -0,0 +1,19 @@
+package src.datatype;
+
+public class Assignment {
+    private Staff staff;
+    private Requirement requirement;
+
+    public Assignment(Staff staff, Requirement requirement) {
+        this.staff = staff;
+        this.requirement = requirement;
+    }
+
+    public String toString() {
+        return staff.getId() + "," + requirement.getId();
+    }
+
+    public String debugString() {
+        return staff.getName() + " is assigned to " + requirement.getId();
+    }
+}
